b7dd7572cc9b50cb7a1b15ed07f2b72611a3fd2c
BhushanGavhane <gavhane1007@gail.com>
2022-08-29 17:49:55 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,37 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string
+
+        }
+        else if(e.target.innerHTML == 'AC') {
+            string = "";
+            document.querySelector('input').value = string
+
+        }
+        else if(e.target.innerHTML == 'C') {
+            string = "0";
+            document.querySelector('input').value = string
+
+        }
+        else if(e.target.innerHTML == '+/-') {
+            string = (-string);
+            document.querySelector('input').value = string
+
+        }
+        else if(e.target.innerHTML == '%') {
+            string = (string*1/100);
+            document.querySelector('input').value = string
+
+        }
+        else{ 
+            console.log('e.target')
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+
+});
